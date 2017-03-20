6b19dd7b30ce91126021bfa75d41bf8a7acabe69
jamesbong187 <jamesbong19@gmail.com>
2017-03-20 10:04:55 +0300

    Update views.py

--- imagepool/views.py
+++ imagepool/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, InvalidPage
 from django.core.urlresolvers import reverse
 import json
 
